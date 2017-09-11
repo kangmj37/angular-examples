6b36a29fab5f0b632e4821eb5c4982c8f1e87a5b
kangmj37 <kangmj37@gmail.com>
2017-09-11 13:30:14 +0900

    Edit reversi

--- src/src-reversi/app/app.component.ts
+++ src/src-reversi/app/app.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Hello World';
+export class AppComponent implements OnInit {
+  title = 'Hello Reversi';
+  blocks: string[];
+  order: string;
 
-  blocks = new Array(64);
+  constructor() {
+    this.order = 'white';
+    this.blocks = new Array(64);
+  }
+
+  ngOnInit() {
+    this.allHide();
+    
+    /* 34, 35, 42, 43 visible */
+    this.setVisible(document.getElementById("34").children[1] as HTMLElement);
+    this.setVisible(document.getElementById("35").children[0] as HTMLElement);
+    this.setVisible(document.getElementById("42").children[0] as HTMLElement);
+    this.setVisible(document.getElementById("43").children[0] as HTMLElement);
+  }
+
+  setHide(element: HTMLElement): void {
+    element.style.visibility = 'hidden';
+  }
+  
+  setVisible(element: HTMLElement): void {
+    element.style.visibility = 'visible';
+  }
+
+  allHide(): void {
+//  this.blocks.forEach((_, idx) => {
+    for (let i = 0; i < this.blocks.length; i++) {
+      let element = document.getElementById(i.toString());
+      for (let i = 0; i < element.children.length; i++)
+        this.setHide(element.children[i] as HTMLElement);
+    }
+//  });
+  }
 }
