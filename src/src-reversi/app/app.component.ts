672ceb0ee6de94f1ff38fcdc10d8ad53cdd77bcc
kangmj37 <kangmj37@gmail.com>
2017-09-15 20:03:36 +0900

    Edit reversi

--- src/src-reversi/app/app.component.ts
+++ src/src-reversi/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+const MATRIX_ROW_SIZE = 8;
+const MATRIX_COL_SIZE = 8;
+
+const BLACK = 'black';
+const WHITE = 'white';
+const EMPTY = '';
+
+class Cell {
+  stat: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,39 +18,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Hello Reversi';
-  blocks: string[];
+  cells: Array<Cell>;
   order: string;
 
-  constructor() {
-    this.order = 'white';
-    this.blocks = new Array(64);
-  }
+  constructor() {}
 
   ngOnInit() {
-    this.allHide();
-    
+    let r_index: number, c_index: number;
+
+	this.order = 'white';
+    this.cells = new Array(MATRIX_ROW_SIZE * MATRIX_COL_SIZE);
+	for (let i = 0; i < MATRIX_ROW_SIZE; i++) {
+	  for (let y = 0; y < MATRIX_COL_SIZE; y++) {
+	    this.cells[i * MATRIX_ROW_SIZE + y] = { stat: EMPTY }
+	  }
+	}
+
     /* 34, 35, 42, 43 visible */
-    this.setVisible(document.getElementById("34").children[1] as HTMLElement);
-    this.setVisible(document.getElementById("35").children[0] as HTMLElement);
-    this.setVisible(document.getElementById("42").children[0] as HTMLElement);
-    this.setVisible(document.getElementById("43").children[0] as HTMLElement);
+	r_index = (MATRIX_ROW_SIZE - 2) / 2;
+	c_index = (MATRIX_COL_SIZE - 2) / 2;
+    
+	this.setWhite(r_index * MATRIX_ROW_SIZE + c_index);
+	this.setBlack(r_index * MATRIX_ROW_SIZE + c_index + 1);
+	this.setBlack((r_index + 1) * MATRIX_ROW_SIZE + c_index);
+	this.setWhite((r_index + 1) * MATRIX_ROW_SIZE + c_index + 1);
   }
 
-  setHide(element: HTMLElement): void {
-    element.style.visibility = 'hidden';
+  private setBlack(index: number): void {
+    this.cells[index].stat = BLACK;
   }
   
-  setVisible(element: HTMLElement): void {
-    element.style.visibility = 'visible';
+  private setWhite(index: number): void {
+    this.cells[index].stat = WHITE;
+  }
+  
+  private setEmpty(index: number): void {
+    this.cells[index].stat = EMPTY;
   }
 
-  allHide(): void {
-//  this.blocks.forEach((_, idx) => {
-    for (let i = 0; i < this.blocks.length; i++) {
-      let element = document.getElementById(i.toString());
-      for (let i = 0; i < element.children.length; i++)
-        this.setHide(element.children[i] as HTMLElement);
-    }
-//  });
+  onClick(index: number): void {
+    let reverseList: Array<number>;
+	
+	if (this.cells[index].stat != EMPTY)
+	  return;
+
+	reverseList = this.hasReverse(index);
+  }
+
+  private hasReverse(index: number): Array<number> {
+    let reverseList: Array<number> = Array(0);
+    let i;
+
+	return reverseList;
   }
 }
