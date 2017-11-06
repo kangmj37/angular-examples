ec2afdefcbc05e4ec4b41d66c654ef0b3f6bdea1
kangmj37 <kangmj37@gmail.com>
2017-11-06 13:21:44 +0900

    edit decorators.ts

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,15 +1,19 @@
 export function trace(enable: boolean, prefix: string = '') {
+	console.log();
+	console.log(`Trace call enable:${enable} - prefix:${prefix}`);
+	
 	return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
 		const originMethod = descriptor.value;
 		descriptor.value = function(...args) {
-			console.log(`${prefix}>>> ENTER HM::${propertyKey}`);
-			//originMethod(args);
+			if (enable) console.log(`${prefix}>>> ENTER HM::${propertyKey}`);
+			// How ???
+			// originMethod(args);
 			console.log('METHOD call');
-			console.log(`${prefix}<<< EXIT HM::${propertyKey}`);
+			if (enable) console.log(`${prefix}<<< EXIT HM::${propertyKey}`);
 
 		}
 
-		console.log(`Trace call ${prefix} - ${propertyKey}`);
+		console.log(`Trace function call ${prefix} - ${propertyKey}`);
 
 		return descriptor;
 	}
